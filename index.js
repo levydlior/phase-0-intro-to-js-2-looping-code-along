211c69eae8ec27f62ec85b04edc8de0e69e189c4
Lior <levydlior@gmail.com>
2022-02-05 18:41:52 -0700

    commit tree 5a0ba4923ab414e02bf2059db2e1833f84fa9d3a

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+/*for (let age= 30; age<40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }
+
+  //for with array:
+  const gifts= ["teddey bear", "drone", "doll"];
+  function wrapGifts(gifts){
+      for(let i= 0; i< gifts.length; i++){
+          console.log(`Wrapped ${gifts[i]} and added a bowl!`);
+      }
+      return gifts;
+  }
+
+  wrapGifts(gifts)
+  */
+
+  function writeCards (arr, eventName){
+    const newArr= [];
+    for (let i = 0; i<arr.length; i++){
+        
+newArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+    }  
+    return newArr;
+  }
+
+  function countDown(x){
+let count= x;
+while (x >= 0){
+    console.log(x);
+    x--;
+}
+return count
+  }
+
+  console.log(countDown(5))
